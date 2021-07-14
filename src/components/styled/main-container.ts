9957a31bcefaf3fea19e6348435907574cfceeab
B'Tunde <tunde1990@gmail.com>
2021-07-14 02:20:30 +0200

    feat(): styled component

---
+++ src/components/styled/main-container.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const MainContainer = styled.main`
+  color: #232129;
+  font-family: -apple-system, Roboto, sans-serif, serif;
+`;
+
+export { MainContainer };
